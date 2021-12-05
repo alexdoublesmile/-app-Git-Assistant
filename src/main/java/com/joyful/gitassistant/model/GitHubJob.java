554436435423c04b45fef9b8738e41161b1f09c2
Александр Плохой <plohoy@zen-it.ru>
2021-12-05 14:08:30 +0300

    feat(github): add github token api

--- src/main/java/com/joyful/gitassistant/model/GitHubJob.java
+++ src/main/java/com/joyful/gitassistant/model/GitHubJob.java
@@ -1,4 +1,22 @@
 package com.joyful.gitassistant.model;
 
+import org.kohsuke.github.GitHub;
+import org.kohsuke.github.GitHubBuilder;
+
+import java.io.IOException;
+
 public class GitHubJob {
+    private final GitHub gitHub;
+
+    public GitHubJob() {
+
+        try {
+            gitHub = new GitHubBuilder()
+                    .withAppInstallationToken(System.getenv("GITHUB_TOKEN"))
+                    .build();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
